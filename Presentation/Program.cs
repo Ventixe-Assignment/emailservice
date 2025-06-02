5a14244e95fa3b2b808761c8c7240b72c1d12968
Janne <tapioninetytwo@live.se>
2025-06-02 18:18:08 +0200

    testing to see if I needed to change environmentvariable

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -5,7 +5,7 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(builder.Configuration["VaultUri"]!);
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri")!);
 builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
